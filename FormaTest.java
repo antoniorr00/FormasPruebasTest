569c4f474604c419d4f51e4510ff6e273d2f24ad
antoniorr00 <antoniorr00.arr@gmail.com>
2025-03-24 08:34:58 +0100

    Update FormaTest.java

--- FormaTest.java
+++ FormaTest.java
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class FormaTest {
     @Test
     void testFormaToString(){
-        Forma cilindro = new Cilindro("cilindro", 6, 21);
+        Forma cilindro = new Cilindro("cilindro", 10, 30);
         String salidaEsperada = "Forma: cilindro";
         String salidaRecibida = cilindro.toString();
         assertEquals(salidaEsperada, salidaRecibida);
 
-        Forma esfera = new Esfera("esfera", 6);
+        Forma esfera = new Esfera("esfera", 15);
         salidaEsperada = "Forma: esfera";
         salidaRecibida = esfera.toString();
         assertEquals(salidaEsperada, salidaRecibida);
 
-        Forma rectangulo = new Rectangulo("rectangulo", 18, 7);
+        Forma rectangulo = new Rectangulo("rectangulo", 35, 20);
         salidaEsperada = "Forma: rectangulo";
         salidaRecibida = rectangulo.toString();
         assertEquals(salidaEsperada, salidaRecibida);
@@ -25,19 +25,19 @@ class FormaTest {
 
     @Test
     void testFromaDevolverArea(){
-        Forma cilindro = new Cilindro("cilindro", 6, 21);
-        double salidaEsperada = Math.PI * 6 * 6 * 21;
+        Forma cilindro = new Cilindro("cilindro", 10, 30);
+        double salidaEsperada = 9424.777;
         double salidaRecibida = cilindro.area();
-        assertEquals(salidaEsperada, salidaRecibida);
+        assertEquals(salidaEsperada, salidaRecibida, 0.001);
 
-        Forma esfera = new Esfera("esfera", 6);
-        salidaEsperada = 4 * Math.PI * 6 * 6;
+        Forma esfera = new Esfera("esfera", 15);
+        salidaEsperada = 2827.433;
         salidaRecibida = esfera.area();
-        assertEquals(salidaEsperada, salidaRecibida);
+        assertEquals(salidaEsperada, salidaRecibida, 0.001);
 
-        Forma rectangulo = new Rectangulo("rectangulo", 10, 5);
-        salidaEsperada = 10 * 5;
+        Forma rectangulo = new Rectangulo("rectangulo", 35, 20);
+        salidaEsperada = 700.0;
         salidaRecibida = rectangulo.area();
-        assertEquals(salidaEsperada, salidaRecibida);
+        assertEquals(salidaEsperada, salidaRecibida, 0.001);
     }
 }
